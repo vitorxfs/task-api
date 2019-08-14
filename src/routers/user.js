55399eb9059fba05851a14248553eb40d80b561e
vitorxfs <viisanches98@gmail.com>
2019-08-14 15:57:37 -0300

    Set test assertions with Jest

--- src/routers/user.js
+++ src/routers/user.js
@@ -31,7 +31,7 @@ router.post('/users/login', async (req, res) => {
         const token = await user.generateAuthToken()
         res.send({ user, token })
     }catch (e){
-        res.status(400).send()
+        res.status(400).send(error)
 
     } 
 })
